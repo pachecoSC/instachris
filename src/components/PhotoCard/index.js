eec996426dede897af52ef1b521dae924f536634
pachecoSC <pachecoschris@gmail.com>
2021-06-16 15:46:14 -0500

    se implemento consumo de like y dislike con token de sesion. pendiente loader

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { ImgWrapper, Img, Articulo } from './styles'
 // import { MdFavoriteBorder,MdFavorite } from "react-icons/md";
 import { FavButtons } from '../FavButtons/index'
-import { useLocalStorage } from '../../hooks/useLocalStorage'
+// import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 // import { useMutationToogleLike } from '../../container/ToggleLikeMutation'
 import { useMutationToogleLike } from '../../container/ToggleLikeMutation'
@@ -12,9 +12,9 @@ import { Link } from '@reach/router'
 const DEFAULT_IMAGE =
   'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const key = `like-${id}`
-  const [liked, setLiked] = useLocalStorage(key, false)
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
+  // const key = `like-${id}`
+  // const [liked, setLiked] = useLocalStorage(key, false)
   // console.log(liked)
   const [show, ref] = useNearScreen()
 
@@ -22,12 +22,13 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   // console.log('{ mutation, mutationLoading, mutationError }', { mutation, mutationLoading, mutationError })
   // const handleFavClick = () => setLiked(!liked)
   const handleFavClick = () => {
-    !liked && mutation({ // para que ejecute solo cuando da like no cada vez que da click al boton.
+    // !liked &&
+    mutation({ // para que ejecute solo cuando da like no cada vez que da click al boton.
       variables: {
         input: { id }
       }
     })
-    setLiked(!liked)
+    // setLiked(!liked)
   }
   /* un ejemplo de como podria funcionar.
   const [toggleLike] = useMutationToogleLike()
--- src/index.js
+++ src/index.js
@@ -1,16 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import ApolloClient from 'apollo-boost'
-// import { ApolloProvider } from '@apollo/react-hooks'
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client'
 import { App } from './App'
 
 import Context from './Context'
 
-const client = new ApolloClient({
-  uri: 'https://instachris-server.vercel.app/graphql',
-  cache: new InMemoryCache()
+import { setContext } from '@apollo/client/link/context'
+import { onError } from '@apollo/client/link/error'
+
+const httpLink = createHttpLink({
+  uri: 'https://instachris-server.vercel.app/graphql'
 })
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = window.sessionStorage.getItem('token')
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+  onError: onError(({ networkError }) => {
+    if (networkError && networkError.result.code === 'invalid_token') {
+      window.sessionStorage.removeItem('token')
+      window.location = '/user'
+    }
+  })
+})
+
+// const client = new ApolloClient({
+//   uri: 'https://instachris-server.vercel.app/graphql',
+//   cache: new InMemoryCache()
+// })
 // ReactDOM.render(<h1>Hola Pacheco</h1>, document.getElementById('app'))
 ReactDOM.render(
   <Context.Provider>
