601e01d232844554ee63f58916d2076e257b5252
pacheco <pachecoschris@gmail.com>
2022-02-10 13:35:55 -0500

    Updates

---
+++ src_pages_notFound_js.app.bundle.js
@@ -0,0 +1,22 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkcurso_platzi_react_avanzado"] = self["webpackChunkcurso_platzi_react_avanzado"] || []).push([["src_pages_notFound_js"],{
+
+/***/ "./src/pages/notFound.js":
+/*!*******************************!*\
+  !*** ./src/pages/notFound.js ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n // const NotFound =\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (function () {\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"h1\", null, \"404\", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"p\", null, \"esta pagina no existe\"));\n});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvcGFnZXMvbm90Rm91bmQuanMuanMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9jdXJzby1wbGF0emktcmVhY3QtYXZhbnphZG8vLi9zcmMvcGFnZXMvbm90Rm91bmQuanM/MzU0NSJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgUmVhY3QgZnJvbSAncmVhY3QnXHJcbi8vIGNvbnN0IE5vdEZvdW5kID1cclxuZXhwb3J0IGRlZmF1bHQgKCkgPT4gKFxyXG4gIDxoMT5cclxuICAgIDQwNDxwPmVzdGEgcGFnaW5hIG5vIGV4aXN0ZTwvcD5cclxuICA8L2gxPlxyXG4pXHJcbiJdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQTtBQUNBO0FBQ0E7QUFBQTtBQUFBIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/pages/notFound.js\n");
+
+/***/ })
+
+}]);
